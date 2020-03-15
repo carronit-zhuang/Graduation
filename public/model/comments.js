6ffd8c3b6a06d8bb0131085e845f42825b49470c
carronit.zhuang <carronit.zhuang@gz.questwork.com>
2020-03-15 23:17:21 +0800

    -optimized the style of comment component -remark unused code

--- public/model/comments.js
+++ public/model/comments.js
@@ -2,7 +2,7 @@ const mongoose = require('./db.js')
 
 const CommnetsSchema = mongoose.Schema({
   "id":String,
-  "message":Object,
-  "sort":Number
+  "sort": Number,
+  "message":Object
 })
 module.exports = mongoose.model('Comments', CommnetsSchema, 'comments')
--- src/components/subcomponents/comment.vue
+++ src/components/subcomponents/comment.vue
@@ -6,16 +6,33 @@
 
    <mt-button type='primary' size='large' @click='postComment'>发表评论</mt-button>
    <div class="cmt-list">
-     <div class="cmt-item" v-for="(item,i) in comments" :key='item._id'>
-       <div class="cmt-title">
+     <div class="cmt-item" v-for="(item, i) in comments" :key='item.index'>
+       <!-- <div class="cmt-title">
          <span>第{{i+1}}楼&nbsp;&nbsp; 用户： {{item.message.user_name}}&nbsp;&nbsp;</span> <span>发表时间:{{item.message.add_time | dateFormat}}</span>
        </div>
        <div class="cmt-body">
          {{item.message.content === 'undefined' ? '此用户很懒，什么都没留下':item.message.content}}
-       </div>
+       </div> -->
+       		<div class="mui-card">
+				<div class="mui-card-header">
+           <span>第</span> 
+           <span class="order">{{i+1}}</span>
+           <span>楼 ：</span>
+           <span class="author"> {{item.message.user_name}}&nbsp;&nbsp;</span>
+           <div>
+             <span class="release">发表时间:</span>
+             <span class="time">{{item.message.add_time | dateFormat}}</span>
+           </div>
+        </div>
+				<div class="mui-card-content">
+					<div class="mui-card-content-inner">
+						 {{item.message.content === 'undefined' ? '此用户很懒，什么都没留下':item.message.content}}
+					</div>
+				</div>
+			</div>
      </div>
    </div>
-   <mt-button type='danger' size='large' plain @click='getMore'>加载更多</mt-button>
+   <mt-button type='danger' size='large' plain @click='getMore'>点击展开更多评论</mt-button>
 
   </div>
 </template>
@@ -40,8 +57,11 @@ export default {
           let message = []
           message = (JSON.parse(result.bodyText))
           // this.comments = this.comments.concat(result.body.message)
+          if(message.length == 0){
+            return mui.toast('已加载到最后一页！')
+          }
           this.comments = this.comments.concat(message)
-
+          // console.log(this.comments)
       })
     },
     getMore () { // 获取更多
@@ -51,21 +71,30 @@ export default {
     postComment () { // 发表评论
       // 校验提交的内容是否为空
       if (this.msg.trim().length === 0) {
-        return Toast('评论内容不能为空！')
+        return mui.toast('评论内容不能为空！')
       }
+      mui.toast('发表成功')
       // post的三个参数：
       // 1.请求的url地址
       // 2.提交给服务器的数据对象 {content: this.msg}
       // 3.定义提交的时候，表单中数据的格式 {emulateJSON: true}   这个应该全局配置，比较方便
       this.$http
-        .post('api/postcomment/' + this.$route.params.id, { content: this.msg.trim()}).then(function (result) {
+        .post('api/postcomment/' + this.$route.params.id, { content: this.msg.trim()}).then(result => {
           // 1.拼接出一个评论对象
-
           // var cmt = { user_name: '匿名用户', add_time: Date.now(), content: this.msg.trim() }
           // // 2.将拼接出的评论放到数组的第一项
           // this.comments.unshift(cmt)
           this.msg = ''
-          this.getComments()
+          this.pageIndex = 1
+          // let message = []
+          // message = (JSON.parse(result.bodyText))
+          // this.comments = message
+          this.$http.
+            get('api/getcomments/' + this.id + '/' + this.pageIndex).then(result => {
+          let message = []
+          message = (JSON.parse(result.bodyText))
+          this.comments = message
+          })
       })
     }
 
@@ -84,23 +113,49 @@ export default {
       height: 85px;
       margin: 0;
     }
-    .cmt-list{
-      margin: 5px 0;
-      .cmt-item{
-        font-size: 13px;
-        .cmt-title{
-          display: flex;
-          padding:0 10px;
-          justify-content:space-between;
-          line-height: 30px;
-          background-color: darkseagreen;
-        }
-        .cmt-body{
-          line-height: 35px;
-          text-indent: 2em;
-        }
+  .mui-card{
+    border:1px solid goldenrod;
+    .mui-card-header{
+      display: flex;
+      justify-content: space-between;
+      font-size: 13px;
+      border-bottom:1px solid rgb(209, 136, 41);
+      .order{
+        color: rgb(155, 27, 180);
+      }
+      .author{
+        color: rgb(204, 62, 27);
+        margin-left: -15px;
+      }
+      .release{
+        margin-right: 3px;
+      }
+      .time{
+        color: rgb(63, 161, 168);
       }
     }
+    .mui-card-content{
+      color: grey;
+    }
+  }
+    
+    // .cmt-list{
+    //   margin: 5px 0;
+    //   .cmt-item{
+    //     font-size: 13px;
+    //     .cmt-title{
+    //       display: flex;
+    //       padding:0 10px;
+    //       justify-content:space-between;
+    //       line-height: 30px;
+    //       background-color: darkseagreen;
+    //     }
+    //     .cmt-body{
+    //       line-height: 35px;
+    //       text-indent: 2em;
+    //     }
+    //   }
+    // }
 
   }
 </style>
