27825f02d0e48cde1de11497991c12b51eaeaaae
carronit.zhuang <carronit.zhuang@gz.questwork.com>
2020-03-15 23:22:05 +0800

    -change style of newslist : added right arrow

--- src/components/news/NewsList.vue
+++ src/components/news/NewsList.vue
@@ -1,6 +1,19 @@
 <template>
   <div>
-    <ul class="mui-table-view">
+    <ul class="mui-table-view mui-table-view-chevron">
+				<li class="mui-table-view-cell mui-media" v-for="item in newslist" :key='item.id'>
+        <router-link :to=" '/home/newsinfo/' + item.id">
+					<a class="mui-navigate-right">
+						<img class="mui-media-object mui-pull-left" :src="item.img_url">
+						<div class="mui-media-body">
+							<h1>{{item.title}}</h1>
+							<p class='mui-ellipsis'><span>发表时间：{{item.add_time | dateFormat}}</span></p>
+						</div>
+					</a>
+				</router-link>
+				</li>
+			</ul>
+    <!-- <ul class="mui-table-view">
 				<li class="mui-table-view-cell mui-media" v-for="item in newslist" :key='item.id'>
 					<router-link :to=" '/home/newsinfo/' + item.id">
 						<img class="mui-media-object mui-pull-left" :src="item.img_url">
@@ -14,7 +27,7 @@
 					</router-link>
 				</li>
 
-			</ul>
+			</ul> -->
 
   </div>
 </template>
@@ -44,6 +57,7 @@ export default {
 <style lang="scss" scoped>
   .mui-table-view {
     h1 {
+    color: black;
     font-size:  14px;
     }
     .mui-ellipsis {
