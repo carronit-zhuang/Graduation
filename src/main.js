cd1e7e17537c2eb2217e169285cabfc5731a302e
carronit.zhuang <carronit.zhuang@gz.questwork.com>
2020-04-16 16:17:23 +0800

    -added vuerify to validate data and set Localstorage

--- src/main.js
+++ src/main.js
@@ -3,8 +3,17 @@ import Vue from 'vue'
 // 1.1 导入路由的包
 import VueRouter from 'vue-router'
 
-// import address from 'address'
 
+import Vuerify from 'vuerify'
+
+Vue.use(Vuerify, {
+  phone: {
+    test: /^1[3456789]\d{9}$/,
+    message: '请您输入正确的手机号码！'
+  }})/*, 添加自定义规则，默认提供了 email, required, url 等规则 */
+
+// import  VConsole  from  'vconsole'
+// new VConsole()
 // 注册vuex状态管理器
 import Vuex from 'vuex'
 // 1.3 引入自己的router.js路由模块
@@ -13,7 +22,7 @@ import router from './router.js'
 import VuePreview from 'vue-preview'
 // 导入格式化时间的moment插件
 import moment from 'moment'
-
+import 'lib-flexible'
 // 2.1 导入vue-resource
 import VueResource from 'vue-resource'
 // 引入app组件
@@ -25,7 +34,7 @@ import MintUi from 'mint-ui'
 
 // 导入MUI的样式
 import './lib/mui/css/mui.css'
-import './lib/mui/css/mui.min.css'
+// import './lib/mui/css/mui.min.css'
 
 import './lib/mui/css/icons-extra.css'
 import '../public/global.css'
@@ -47,23 +56,31 @@ import mui from './lib/mui/js/mui.js'
 Vue.use(Vuex)
 Vue.prototype.mui = mui
 // 每次刚进入网站，肯定会调用main.js，在刚调用的时候，先从本的存储中把购物车的数据读出来，放到Store中
-var car = JSON.parse(localStorage.getItem('car') || '[]')
+let {cart} = JSON.parse(localStorage.getItem('Login_data') || '[]')
+if(cart){
+  if(!Array.isArray(cart)){
+    cart = JSON.parse(cart)
+  }
+}else{
+  cart = []
+}
+// var cart = JSON.parse(localStorage.getItem('Login_data')).cart
 var store = new Vuex.Store({
   state: {// this.$store.state.***
-    car: car // 将购物车中的商品的数据，用一个数组存储起来，在car数组中，存储一些商品的对象，咱们可以将这个商品的对象设计成如下这个样子：
+    'cart': cart, // 将购物车中的商品的数据，用一个数组存储起来，在cart数组中，存储一些商品的对象，咱们可以将这个商品的对象设计成如下这个样子：
     // {id:商品的id , count:要购买的数量,price: 商品的单价, selected: false}
-
+    'order': []
   },
   mutations: {// this.$store.commit('方法的名称'，'按需传递唯一的参数')
-    addToCar (state, goodsinfo) {
-      // 点击加入购物车时，把商品的信息保存到store中的car上
+    addToCart (state, goodsinfo) {
+      // 点击加入购物车时，把商品的信息保存到store中的cart上
       // 分析：
       // 1.如果购物车中，之前早就有这个对应的商品了，那么只需要更新数量
-      // 2.如果没有，则直接把商品的数据push到car中即可
+      // 2.如果没有，则直接把商品的数据push到cart中即可
 
       // 假设在购物车中，没有找到对应的商品
       var flag = false
-      state.car.some(item => {
+      state.cart.some(item => {
         if (item.id === goodsinfo.id) {
           item.count += parseInt(goodsinfo.count)
           flag = true
@@ -72,43 +89,68 @@ var store = new Vuex.Store({
       })
       // 如果最终循环检查完毕，没有发现相同的商品，那么得到的flag就还是false，则把商品的数据对象直接push到购物车中
       if (!flag) {
-        state.car.push(goodsinfo)
+        state.cart.push(goodsinfo)
       }
-      // 每当更新了car之后，要记得把car数组存储到本地的loaclStorage中
-      localStorage.setItem('car', JSON.stringify(state.car))
+      // 每当更新了cart之后，要记得把cart数组存储到本地的loaclStorage中
+      // localStorage.setItem('cart', JSON.stringify(state.cart))
+      let tempObj = JSON.parse(localStorage.getItem('Login_data'))
+      tempObj.cart = state.cart
+      localStorage.setItem('Login_data', JSON.stringify(tempObj))
     },
     updateGoodsInfo (state, goodsinfo) {
       // 修改Vuex里购物车中商品的数量
-      state.car.find(item => {
+      state.cart.find(item => {
         if (item.id === goodsinfo.id) {
           item.count = parseInt(goodsinfo.count)
           return true
         }
       })
       // 当修改完商品的数量，就把最新的的数量保存到本地存储中
-      localStorage.setItem('car', JSON.stringify(state.car))
+      let tempObj = JSON.parse(localStorage.getItem('Login_data'))
+      tempObj.cart = state.cart
+      localStorage.setItem('Login_data', JSON.stringify(tempObj))
+      // localStorage.setItem('cart', JSON.stringify(state.cart))
     },
     removeData (state, id) {
       // 根据传过来的id删除store中那条数据
-      state.car.find((item, index) => {
+      state.cart.find((item, index) => {
         if (item.id === id) {
-          state.car.splice(index, 1)
+          state.cart.splice(index, 1)
           return true
         }
       })
       // 将删除完毕后的最新数据重新写到本地存储中
-      console.log(JSON.stringify(state.car))
-      localStorage.setItem('car', JSON.stringify(state.car))
+      // console.log(JSON.stringify(state.cart))
+      // localStorage.setItem('cart', JSON.stringify(state.cart))
+      let tempObj = JSON.parse(localStorage.getItem('Login_data'))
+      tempObj.cart = state.cart
+      localStorage.setItem('Login_data', JSON.stringify(tempObj))
     },
     updateGoodsSelected (state, info) {
-      state.car.find(item => {
+      state.cart.find(item => {
         if (item.id === info.id) {
           item.selected = info.selected
           return true
         }
       })
       // 更改了selected属性之后，再把更新后的开关状态保存到本地存储中去
-      localStorage.setItem('car', JSON.stringify(state.car))
+      // localStorage.setItem('cart', JSON.stringify(state.cart))
+      let tempObj = JSON.parse(localStorage.getItem('Login_data'))
+      tempObj.cart = state.cart
+      localStorage.setItem('Login_data', JSON.stringify(tempObj))
+    },
+    updateCart(state, array){
+      return state.cart = array
+    },
+    clearAll(state){
+      state.cart = []
+    },
+    assignStateOrder(state,obj){
+      if(!obj){
+        return state.order = state.cart.filter(item=>item.selected == true)
+      }
+      state.order = []
+      return state.order.push(obj)
     }
 
   },
@@ -116,21 +158,23 @@ var store = new Vuex.Store({
     // 相当于计算属性，也相当于filters，做了一层包装
     getAllCount (state) {
       var c = 0
-      state.car.forEach(item => {
-        c += item.count
+      state.cart.forEach(item => {
+        if(item.selected){
+          c += item.count
+        }
       })
       return c
     },
     getGoodsCount (state) {
       var o = {}
-      state.car.forEach(item => {
+      state.cart.forEach(item => {
         o[item.id] = item.count
       })
       return o
     },
     getGoodsSelected (state) {
       let o = {}
-      state.car.forEach(item => {
+      state.cart.forEach(item => {
         o[item.id] = item.selected
       })
       return o
@@ -140,7 +184,7 @@ var store = new Vuex.Store({
         count: 0,
         totalPrice: 0
       }
-      state.car.forEach(item => {
+      state.cart.forEach(item => {
         if (item.selected) {
           o.count += item.count
           o.totalPrice += item.count * item.price
@@ -160,10 +204,15 @@ Vue.filter('dateFormat', function (dataStr, pattern = 'YYYY-MM-DD HH:MM:SS') {
 })
 // 2.2 安装vue-resource
 Vue.use(VueResource)
-// const ip = address.ip()
+
+
 //全局配置请求的根路径
 // Vue.http.options.root = 'http://www.liulongbin.top:3005'
-Vue.http.options.root = 'http://localhost:3000'
+// Vue.http.options.root = 'http://192.168.1.105:3005'
+Vue.http.options.root = '/zjj'
+
+// const ip = getIpAddress()
+
 // Vue.http.options.root = `http://${ip}:3000`
 
 
