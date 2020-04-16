cd52a04632794ffcac0f97d28bd17398c0179a77
carronit.zhuang <carronit.zhuang@gz.questwork.com>
2020-04-16 16:13:01 +0800

    -added API and related service func

--- public/model/router.js
+++ public/model/router.js
@@ -11,6 +11,56 @@ router.get('/api/getnew/:id' ,service.getNewsInfo);
 router.get('/api/getcomments/:id/:pageindex',service.getComments);
 // 发表评论
 router.post('/api/postcomment/:id',service.addComments);
+//获取图片分享的分类信息
+router.get('/api/getimgcategory' ,service.getImgCategory);
+//获取图片列表预览信息
+router.get('/api/getimages/:cateId' ,service.getImgList);
+//获取图片列表详情
+router.get('/api/getimageInfo/:id' ,service.getPhotoInfo);
+//获取缩略图信息
+router.get('/api/getthumbnail/:id' ,service.getThumbnail);
+//获取商品列表预览信息
+// router.get('/api/getgoods/:pageindex' ,service.getGoodsList);
+//获取商品详情的信息
+router.get('/api/goods/getinfo/:id' ,service.getGoodsInfo);
+//获取轮播图信息
+router.get('/api/getswiper/:id' ,service.getSwiper);
+//获取商品图文介绍信息desc
+router.get('/api/goods/getdesc/:id' ,service.getGoodsDesc);
+//获取商品评论页面的信息
+router.get('/api/home/goodscomment/:id/:pageindex' ,service.getComments);
+//获取商品列表的分类信息
+router.get('/api/getgoodscategory' ,service.getGoodsCategory);
+//获取相应分类商品的信息
+router.get('/api/getgoodswithcate/:cate/:pageindex' ,service.getGoodsListWithCate);
+//获取购物车列表的信息
+router.get('/api/goods/getshopcarlist/:id' ,service.getShopCarList);
+//提交用户账号注册的信息
+router.post('/api/account/register' ,service.accountRegister);
+//查找用户账号信息,同是进行账户是否存在的判断
+router.get('/api/account' ,service.getAccount);
+//直接获取用户的账号信息
+router.get('/api/account/straight' ,service.getAccountStraight);
+//验证用户账号信息
+router.get('/api/account/verify' ,service.accountVerify);
+//重设用户密码
+router.put('/api/account/reset',service.accountReset);
+//修改用户信息
+router.put('/api/account/modify',service.accountModify);
+//提交用户订单信息
+router.post('/api/orderlist' ,service.createOrderList);
+//获取用户所有订单信息
+router.get('/api/orderlist' ,service.getOrderList);
+//获取用户某条订单的详情信息
+router.get('/api/orderlist/detail/:orderNum' ,service.getOrderListDetail);
+//修改订单是否付款
+router.put('/api/orderlist/detail/:orderNum' ,service.updateOrderListPayment);
+//获取商品列表的分类信息
+router.get('/api/getordercategory' ,service.getOrderCategory);
+//获取相应分类商品的信息
+router.get('/api/getgoodswithcate/:cate/:pageindex' ,service.getGoodsListWithCate);
+//提交相应反馈信息
+router.post('/api/advice' ,service.createAdvice);
 
 // router.post('/books/book',service.addBook);
 // 编辑图书时根据id来查询相应信息
--- public/model/service.js
+++ public/model/service.js
@@ -1,8 +1,21 @@
 const Newlist = require('./newlist.js')
 const Newsinfo = require('./newsinfo.js')
 const Comments = require('./comments.js')
-const Cmtnew = require('./cmt-new.js')
+const PhotoCate = require('./photo-cate.js')
+const PhotoList = require('./photo-list.js')
+const PhotoInfo = require('./photo-info.js')
+const Thumbnail = require('./thumbnail.js')
+const GoodsList = require('./goods-list.js')
+const Swiper = require('./swiper.js')
+const GoodsDesc = require('./goods-desc.js')
+const GoodsCate = require('./goods-cate.js')
+const ShopCarList = require('./shopcar-list.js')
+const Account = require('./account.js')
+const OrderList = require('./order-list.js')
+const OrderCate = require('./order-cate.js')
+const Advice = require('./advice.js')
 
+//获取新闻列表信息
 exports.getNewlist = (req,res) => {
   Newlist.find({},(err,docs)=>{
     if(err){
@@ -13,6 +26,7 @@ exports.getNewlist = (req,res) => {
   })
 }
 
+//获取新闻详情
 exports.getNewsInfo = (req,res) => {
   let id = req.params.id
   Newsinfo.find({"id": id},(err,docs)=>{
@@ -24,13 +38,31 @@ exports.getNewsInfo = (req,res) => {
   })
 }
 
+//获取评论
 exports.getComments= (req,res) => {
   let pageindex = req.params.pageindex
   let id = req.params.id
-  //新闻资讯页面获取评论数据
-  if(id<=12){
+  if(id<=12){  //新闻资讯页面获取评论数据
     let temp = "news"+id
-    Comments.find({id:temp},(err,docs)=>{
+    Comments.find({'id':temp},(err,docs)=>{
+      if(err){
+        console.log(err)
+        return
+      }
+      res.json(docs)
+    }).sort({"sort":-1}).skip((pageindex-1)*5).limit(5)
+  }else if(id<=26){  //图片详情页面获取评论数据
+    let temp = "photo"+id
+    Comments.find({'id':temp},(err,docs)=>{
+      if(err){
+        console.log(err)
+        return
+      }
+      res.json(docs)
+    }).sort({"sort":-1}).skip((pageindex-1)*5).limit(5)
+  }else if(id<=60){  //商品详情页面获取评论数据
+    let temp = "goods"+id
+    Comments.find({'id':temp},(err,docs)=>{
       if(err){
         console.log(err)
         return
@@ -40,30 +72,68 @@ exports.getComments= (req,res) => {
   }
 }
 
+//新增评论
 exports.addComments= (req,res) => {
   let id = req.params.id
-  let temp = "news"+id
   let content = req.body.content
+  let user_name = req.body.user_name
   let i 
-  // console.log(content)
-    //新闻资讯页面发表评论数据(新增)
     //先拿到已存在的数据数量
-  if(id<=12){
+  if(id<=12){   //新闻资讯页面发表评论数据(新增)
+    let temp = "news"+id
     async function getCount(){
-      await Comments.countDocuments({id:temp},(err,docs)=>{
+      await Comments.countDocuments({'id':temp},(err,docs)=>{
         if(err){
           console.log(err)
           return
         }
          i = docs
       })
-
     //新增数据
-    // console.log(i)
     const comment = new Comments({
       "id":"news"+id,
       "sort":i,
-      "message":{"user_name":"匿名用户","content":content,"add_time":new Date().toISOString() }
+      "message":{"user_name":user_name,"content":content,"add_time":new Date().toISOString() }
+    })
+    comment.save()
+    res.json(i)
+    }
+    getCount()
+  }else if(id<=26){  //图片详情页面发表评论数据(新增)
+    let temp = "photo"+id
+    async function getCount(){
+      await Comments.countDocuments({'id':temp},(err,docs)=>{
+        if(err){
+          console.log(err)
+          return
+        }
+         i = docs
+      })
+    //新增数据
+    const comment = new Comments({
+      "id":"photo"+id,
+      "sort":i,
+      "message":{"user_name":user_name,"content":content,"add_time":new Date().toISOString() }
+    })
+    comment.save()
+    res.json(i)
+    }
+    getCount()
+  }else if(id<=60){  //商品详情页面发表评论数据(新增)
+    let temp = "goods"+id
+    async function getCount(){
+      await Comments.countDocuments({'id':temp},(err,docs)=>{
+        if(err){
+          console.log(err)
+          return
+        }
+         i = docs
+      })
+    //新增数据
+    const comment = new Comments({
+      "id":"goods"+id,
+      "sort":i,
+      "message":{"user_name":user_name,"content":content,"add_time":new Date().toISOString() }
     })
     comment.save()
     res.json(i)
@@ -71,3 +141,382 @@ exports.addComments= (req,res) => {
     getCount()
   }
 }
+
+ //获取图片的分类信息
+exports.getImgCategory = (req,res) => { 
+  PhotoCate.find({},(err,docs)=>{
+    if(err){
+      console.log(err)
+      return
+    }
+    res.json(docs)
+  })
+}
+
+  //获取图片列表
+exports.getImgList = (req,res) => {
+  let cate = req.params.cateId
+  if(cate == 0 ){
+    PhotoList.find({},(err,docs)=>{
+      if(err){
+        console.log(err)
+        return
+      }
+      res.json(docs)
+    })
+  }else{
+    PhotoList.find({"cate":cate},(err,docs)=>{
+      if(err){
+        console.log(err)
+        return
+      }
+      res.json(docs)
+    })
+  }
+}
+
+  //获取图片详情
+exports.getPhotoInfo = (req,res) => {
+  let id = req.params.id
+  PhotoInfo.find({"id": id},(err,docs)=>{
+    if(err){
+      console.log(err)
+      return
+    }
+    res.json(docs)
+  })
+}
+
+  //获取缩略图信息
+exports.getThumbnail = (req,res) => {
+  let id = req.params.id
+  Thumbnail.find({"id": id},(err,docs)=>{
+    if(err){
+      console.log(err)
+      return
+    }
+    res.json(docs)
+  })
+}
+
+  //获取商品列表信息
+// exports.getGoodsList = (req,res) => {
+//   let pageindex = req.params.pageindex
+//   GoodsList.find({},(err,docs)=>{
+//     if(err){
+//       console.log(err)
+//       return
+//     }
+//     res.json(docs)
+//   }).skip((pageindex-1)*4).limit(4)
+// }
+
+  //获取商品详情信息
+exports.getGoodsInfo = (req,res) => {
+  let id = req.params.id
+  GoodsList.find({"id": id},(err,docs)=>{
+    if(err){
+      console.log(err)
+      return
+    }
+    res.json(docs)
+  })
+}
+
+  //获取商品的图文介绍Desc详情信息
+exports.getGoodsDesc = (req,res) => {
+  let id = req.params.id
+  GoodsDesc.find({"id": id},(err,docs)=>{
+    if(err){
+      console.log(err)
+      return
+    }
+    res.json(docs)
+  })
+}
+
+  //获取轮播图信息
+exports.getSwiper = (req,res) => {
+  let id = req.params.id
+  Swiper.find({"id": id},(err,docs)=>{
+    if(err){
+      console.log(err)
+      return
+    }
+    res.json(docs)
+  })
+}
+
+  //获取商品的分类信息
+exports.getGoodsCategory = (req,res) => {
+  GoodsCate.find({},(err,docs)=>{
+    if(err){
+      console.log(err)
+      return
+    }
+    res.json(docs)
+  })
+}
+
+  //获取图片列表
+exports.getGoodsListWithCate = (req,res) => {
+  let cate = req.params.cate
+  let pageindex = req.params.pageindex
+  if(cate == 0 ){
+    GoodsList.find({},(err,docs)=>{
+      if(err){
+        console.log(err)
+        return
+      }
+      res.json(docs)
+    }).skip((pageindex-1)*4).limit(4)
+  }else{
+    GoodsList.find({"cate":cate},(err,docs)=>{
+      if(err){
+        console.log(err)
+        return
+      }
+      res.json(docs)
+    }).skip((pageindex-1)*2).limit(2)
+  }
+}
+
+exports.getShopCarList = (req,res) => {  //获取购物车列表信息
+  let id = req.params.id
+  ShopCarList.find({'id':id},(err,docs)=>{
+    if(err){
+      console.log(err)
+      return
+    }
+    res.json(docs)
+  })
+}
+
+exports.accountRegister = (req,res) => {
+     //新增数据
+  const body = req.body
+  const account = new Account({
+   "username": body.username,
+   "password": body.password,
+   "email": body.email,
+   "phone": body.phone
+  })
+  account.save()
+  res.json(1)
+}
+
+exports.getAccount = (req,res) => {  //获取用户的信息，包括判断
+  const username = req.query.username
+  const password = req.query.password
+  let userExist
+  let correctQuery
+  Account.find({'username':username},(err,docs)=>{
+    if(err){
+      console.log(err)
+      return
+    }
+    userExist = docs.length
+  })
+  Account.find({'username':username,'password':password},(err,docs)=>{
+    if(err){
+      console.log(err)
+      return
+    }
+    correctQuery = docs.length
+    res.json({userExist, correctQuery })
+  })
+
+}
+
+exports.getAccountStraight = (req,res) => {  //直接简单获取用户的信息，不用判断
+  const username = req.query.username
+  Account.find({'username':username},(err,docs)=>{
+    if(err){
+      console.log(err)
+      return
+    }
+    res.json(docs)
+  })
+}
+
+exports.accountVerify = (req,res) => {  //验证用户的信息以继续重设密码
+  const username = req.query.username
+  let userExist
+  let correctQuery
+  if(req.query.phone){
+    verifyValue = req.query.phone
+    Account.find({'username':username, 'phone':verifyValue},(err,docs)=>{
+      if(err){
+        console.log(err)
+        return
+      }
+      correctQuery = docs.length
+    })
+  }else{
+    verifyValue = req.query.email
+    Account.find({'username':username, 'email':verifyValue},(err,docs)=>{
+      if(err){
+        console.log(err)
+        return
+      }
+      correctQuery = docs.length
+    })
+  }
+
+  Account.find({'username':username},(err,docs)=>{
+    if(err){
+      console.log(err)
+      return
+    }
+    userExist = docs.length
+    res.json({userExist, correctQuery })
+  })
+}
+
+  //重设用户密码
+exports.accountReset = (req,res) => {
+let userExist
+const body = req.body
+Account.find({'username':body.username},(err,docs)=>{
+  if(err){
+    console.log(err)
+    return
+  }
+  userExist = docs.length
+
+  Account.updateOne({'username':body.username},{'password': body.password},(err,docs)=>{
+    if(err){
+      console.log(err)
+      return
+    } else if(docs.ok){
+        res.json({userExist})
+      }
+    })
+  })
+}
+
+//修改用户数据
+exports.accountModify = (req,res) => {
+  let userExist
+  const body = req.body
+  if(!body.cart){
+    Account.find({'username':body.username},(err,docs)=>{
+      if(err){
+        console.log(err)
+        return
+      }
+      userExist = docs.length
+      
+      Account.updateOne({'username':body.username},{'username':body.username,'email': body.email,'phone': body.phone},(err,docs)=>{
+        if(err){
+          console.log(err)
+          return
+        } else if(docs.ok){
+            res.json({userExist})
+          }
+        })
+      })
+  }else {
+    Account.find({'username':body.username},(err,docs)=>{
+      if(err){
+        console.log(err)
+        return
+      }
+      userExist = docs.length
+      
+      Account.updateOne({'username':body.username},{'email': body.email,'phone': body.phone,'cart':body.cart},(err,docs)=>{
+        if(err){
+          console.log(err)
+          return
+        } else if(docs.ok){
+            res.json({userExist})
+          }
+        })
+      })
+  }
+}
+
+//新增用户订单数据
+exports.createOrderList = (req,res) => {
+  const body = req.body
+  const account = new OrderList({
+    "username": body.username,
+    "phone": body.phone,
+    "postCode": body.postCode,
+    "specificAddress": body.specificAddress,
+    "orderNum": body.orderNum,
+    "paid": body.paid,
+    "order": body.order
+  })
+  account.save()
+  res.json(1)
+}
+
+exports.getOrderList = (req,res) => {  //直接简单获取用户的信息，不用判断
+  const query = req.query
+  if(typeof query.paid == 'undefined'){
+    OrderList.find({'username':query.username},(err,docs)=>{
+      if(err){
+        console.log(err)
+        return
+      }
+      res.json(docs)
+    })
+  }else{
+    OrderList.find({'username':query.username, 'paid': query.paid},(err,docs)=>{
+      if(err){
+        console.log(err)
+        return
+      }
+      res.json(docs)
+    })
+  }
+}
+
+// 获取某条订单信息的详情
+exports.getOrderListDetail = (req,res) => {  //直接简单获取用户的信息，不用判断
+  const {orderNum} = req.params
+  OrderList.find({'orderNum': orderNum},(err,docs)=>{
+    if(err){
+      console.log(err)
+      return
+    }
+    res.json(docs)
+  })
+}
+
+//更新订单的付款与否信息
+exports.updateOrderListPayment = (req,res) => {
+  const {body} = req
+  const {orderNum} = req.params
+  OrderList.updateOne({'orderNum':orderNum},{'paid': body.paid},(err,docs)=>{
+    if(err){
+      console.log(err)
+      return
+    } else{
+        res.json(docs.ok)
+      }
+    })
+  }
+
+   //获取订单的分类信息
+exports.getOrderCategory = (req,res) => { 
+  OrderCate.find({},(err,docs)=>{
+    if(err){
+      console.log(err)
+      return
+    }
+    res.json(docs)
+  })
+}
+
+//新增留言反馈
+exports.createAdvice = (req,res) => {
+  const body = req.body
+  const advice = new Advice({
+  "account": body.account,
+  "content": body.content,
+  })
+  advice.save()
+  res.json(1)
+  }
